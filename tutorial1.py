cd22018060f8aa781ab07b9095f30378f63e7465
davidwilson826 <davidwilson@hanovernorwichschools.org>
2016-01-25 14:51:06 -0500

    Updated from Brython Server: 1/25/2016 2:51:06 PM

--- tutorial1.py
+++ tutorial1.py
@@ -8,12 +8,14 @@ black = Color(0x000000, 1.0)
 
 thinline = LineStyle(1, black)
 rectangle = RectangleAsset(50, 20, thinline, blue)
-ellipse = EllipseAsset(50, 25, thinline, red)
+ellipse = EllipseAsset(50, 30, thinline, blue)
+polygon = PolygonAsset([(250, 85), (250, 115), (350, 115), (350, 85)], thinline, red)
 
 Sprite(rectangle)
 Sprite(rectangle, (200, 50))
 Sprite(rectangle, (225, 50))
 Sprite(ellipse, (300, 100))
+Sprite(polygon)
 
 myapp = App()
 myapp.run()
